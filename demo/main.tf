5e624267f9e8b12af633b1059005c26d8310cfc4
Simon Brady <simon@hikari.org.nz>
2019-03-07 13:49:18 +1300

    Tidy up code formatting

--- demo/main.tf
+++ demo/main.tf
@@ -1,13 +1,13 @@
 # Resource group to hold the other resources we create
 resource "azurerm_resource_group" "demo" {
-  name = "demo-rg"
+  name     = "demo-rg"
   location = "${var.location}"
 }
 
 # A virtual network resource in the resource group
 resource "azurerm_virtual_network" "demo" {
-  name = "demo-vnet"
+  name                = "demo-vnet"
   resource_group_name = "${azurerm_resource_group.demo.name}"
-  location = "${azurerm_resource_group.demo.location}"
-  address_space = ["${var.address_space}"]
+  location            = "${azurerm_resource_group.demo.location}"
+  address_space       = ["${var.address_space}"]
 }
--- demo/vars.tf
+++ demo/vars.tf
@@ -1,11 +1,11 @@
 # Global variables
 
 variable "location" {
-  default = "Australia East"
+  default     = "Australia East"
   description = "Azure location to create resources in"
 }
 
 variable "address_space" {
-  default = "10.1.0.0/16"
+  default     = "10.1.0.0/16"
   description = "CIDR range to use as virtual network's address space"
 }
